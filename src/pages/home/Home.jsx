e58210b494120f1d6720a08af0194b1ba484a468
greenMakaroni <mrtn.dawid@gmail.com>
2022-08-19 12:13:45 +0100

    create sidebar & navbar components, setup and style sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.scss"
 
 const Home = () => {
   return (
     <>
       <div className="home">
-        <h1 className="title">
-          Dashboard 
-        </h1>
+        <Sidebar />
+        <div className="homeContainer">
+          container
+        </div>
       </div>
-
-      <h1 className="title">
-        Dashboard 
-      </h1>
     </>
 
   )
